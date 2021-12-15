3c90163026fd66ac0db819512af9b9bdd006cdf5
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2021-12-15 20:18:34 +0000

    Solutions added

--- script.js
+++ script.js
@@ -2,39 +2,102 @@
 console.log('Connected!')
 
 //1. Create a function called basicPasswordCheck that takes in a password string and returns "This is a valid password" if the password has 10 or more characters, and "This password is too short" if the password has less than 10 characters.
+function basicPasswordCheck(password){
+	if(password.length >= 10){
+		return "This is a valid password"
+	}
+	else {
+		return "This password is too short"
+	}
+}
+
+console.log(basicPasswordCheck("testingsome"))
+console.log(basicPasswordCheck("password"))
 
 
 
+//2. Create a function called confirmUncommonPassword that checks to make sure that a given password isn't a variation of the two most common passwords: "password1" and "abcd1234". These passwords are easily guessed, so if a user's password includes one of these two phrases (even if there are extra letters or numbers), it should return false. If the user's password does not include one of these two phrases, it should return true.
+function confirmUncommonPassword(password){
+	return password == "password1" ? false
+         : password == "abcd1234" ? false
+         : true;
+}
 
-
-//2. Create a function called confirmUncommonPassword that checks to make sure that a given password isn't a variation of the two most common passwords: "password1" and "abcd1234". These passwords are easily guessed, so if a user's password includes one of these two phrases (even if there are extra letters or numbers), it should return false. If the user's password does not include one of these two phrases, it should return false.
-
-
-
+console.log(confirmUncommonPassword("password1"))
+console.log(confirmUncommonPassword("password"))
 
 
 //3. Create a function called confirmSamePasswords that checks to make sure that two inputted strings are the same. If the two strings are the same, it should return true. If the two strings are different, it should return false.
-
+function confirmSamePasswords(string1, string2){
+	if(string1 == string2){
+		return true
+	}
+	else {
+		return false
+	}
+}
 
 
 
 
 // MEDIUM
 //4. Some older databases can't handle special characters like spaces, @, #, $, etc. Create a function called checkInvalidCharacters that checks to see if a string contains any of the following invalid characters: (a space), !, @, #, or $. If the string contains one of those characters, it should return true. If the string doesn't contain one of those characters, it should return false.
+function checkInvalidCharacters(password){
+	if(password.includes("@") || password.includes("!") || password.includes(" ") || password.includes("#") || password.includes("$") ){
+		return true
+	}
+	return false
+}
 
 
-
+console.log(checkInvalidCharacters("@testing"))
+console.log(checkInvalidCharacters(" testing"))
+console.log(checkInvalidCharacters("testing"))
 
 
 //5. On the other hand, newer sites require that users include one or more special characters in their passwords. Create a function called requireSpecialCharacters that checks to see if a string contains two or more of the following special characters: (a space), !, @, #, $, %, ^, &, -, _, or *. If the string contains two or more of those characters, it should return true. If the string contains one or fewer of those characters, it should return false.
+function requireSpecialCharacters(password){
+	let sum = 0;
+	for(let i = 0; i < password.length; i++){
+		if(password[i].includes("@") || password[i].includes("!") || password[i].includes(" ") || password[i].includes("#") || password[i].includes("$") || password[i].includes("^") || password[i].includes("%") || password[i].includes("-") || password[i].includes("_") || password[i].includes("*")){
+			sum++
+		}
+	}
+	if(sum >= 2){
+		return true
+	}
+	return false
+}
 
 
 
 
 //6. Now write a function called mediumPasswordGenerator that generates a random password and takes in two arguments: the total length of the password and the number of special characters (see #5) the password should have. If the second argument is greater than the first, the function should return false.
+function mediumPasswordGenerator(size, numSpecial){
+	if(numSpecial > size){
+		return false
+	}
+	let password = ""
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	const specialCharacters = "@!#$-_* ^%"
+	const specialLength = specialCharacters.length
+  const charactersLength = characters.length;
+	for(let i = 0; i < size; i++){
+		password += characters.charAt(Math.floor(Math.random() * charactersLength))
+	}
+	for(let i = 0; i < numSpecial; i++){
+		let character = specialCharacters.charAt(Math.floor(Math.random() * specialLength))
+		let randomIndex = Math.floor(Math.random()*password.length)
+		// For some reason this part isn't working
+		console.log(character)
+		password[randomIndex] = character
+		console.log(password)
+	}
 
+	return password
+}
 
-
+console.log(mediumPasswordGenerator(8, 1))
 
 
 
